d3215fdb29cd80d5ee1892d6e24457b4f14eab47
Juan David Sánchez <juancho85@gmail.com>
2017-10-09 22:17:36 +0200

    using repositories

---
+++ src/main/java/com/juandavidsanchez/spring5webapp/model/repositories/AuthorRepository.java
@@ -0,0 +1,7 @@
+package com.juandavidsanchez.spring5webapp.model.repositories;
+
+import com.juandavidsanchez.spring5webapp.model.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+}
---
+++ src/main/java/com/juandavidsanchez/spring5webapp/model/repositories/BookRepository.java
@@ -0,0 +1,7 @@
+package com.juandavidsanchez.spring5webapp.model.repositories;
+
+import com.juandavidsanchez.spring5webapp.model.Book;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+}
